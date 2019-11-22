7377b11dffe80586ae045428bb2401929563aeff
Håkon Wallann <hwa018@post.uit.no>
2019-11-22 11:48:32 +0100

    Add styling and login design to the tutorial view

--- graph_tutorial/tutorial/views.py
+++ graph_tutorial/tutorial/views.py
@@ -4,6 +4,21 @@ from django.shortcuts import render
 
 from django.http import HttpResponse, HttpResponseRedirect
 
+def initialize_context(request):
+  context = {}
+
+  # Check for any errors in the session
+  error = request.session.pop('flash_error', None)
+
+  if error != None:
+    context['errors'] = []
+    context['errors'].append(error)
+
+  # Check for user in the session
+  context['user'] = request.session.get('user', {'is_authenticated': False})
+  return context
+
 def home(request):
-  # Temporary!
-  return HttpResponse("Welcome to the tutorial.")
+  context = initialize_context(request)
+
+  return render(request, 'tutorial/home.html', context)
