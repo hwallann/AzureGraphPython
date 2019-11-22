a87396c78c21320042ec75620441f5618cb8314f
Håkon Wallann <hwa018@post.uit.no>
2019-11-22 11:42:14 +0100

    Add a tutorial folder to the Django server

--- graph_tutorial/tutorial/views.py
+++ graph_tutorial/tutorial/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse, HttpResponseRedirect
+
+def home(request):
+  # Temporary!
+  return HttpResponse("Welcome to the tutorial.")
